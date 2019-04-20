4e59e7df19102afdc325645b173759f7b1aa939d
GeovanaRamos <geovannna.1998@gmail.com>
2019-04-20 11:23:37 -0300

    Corrected fragment subjects list view update by asynctask

--- app/src/main/java/com/unigrade/app/View/AsyncTask/GetSubjects.java
+++ app/src/main/java/com/unigrade/app/View/AsyncTask/GetSubjects.java
@@ -3,60 +3,47 @@ package com.unigrade.app.View.AsyncTask;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.unigrade.app.Controller.SubjectsController;
 import com.unigrade.app.Model.Subject;
+import com.unigrade.app.R;
 import com.unigrade.app.View.Fragment.SubjectsFragment;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class GetSubjects extends AsyncTask<String, Integer, List> {
+public class GetSubjects extends AsyncTask<String, Integer, ArrayList<Subject> > {
 
     private SubjectsController subjectsController;
     private SubjectsFragment subjectsFragment;
-    private ArrayAdapter<Subject> adapter;
-    private ArrayList<Subject> subjects;
 
-    private boolean loading;
-    private ProgressBar progressBar;
-
-    public GetSubjects(SubjectsFragment subjectsFragment, ArrayAdapter<Subject> adapter, boolean loading, ProgressBar progressBar){
-        this.loading = loading;
+    public GetSubjects(SubjectsFragment subjectsFragment){
         this.subjectsFragment = subjectsFragment;
-        this.adapter = adapter;
-        this.progressBar = progressBar;
     }
 
     @Override
     protected void onPreExecute() {
-        subjectsController =  subjectsController.getInstance();
-
-        if(loading){
-            progressBar.setVisibility(View.VISIBLE);
-        }
-        super.onPreExecute();
+        subjectsController =  SubjectsController.getInstance();
     }
 
     @Override
-    protected List doInBackground(String... params) {
+    protected ArrayList<Subject> doInBackground(String... params) {
         return subjectsController.getSubjectsList();
     }
 
     @Override
-    protected void onProgressUpdate(Integer... values) {
+    protected void onPostExecute(ArrayList<Subject> subjects) {
         subjectsFragment.setSubjects(subjects);
-        adapter.notifyDataSetChanged();
-    }
+        View v = subjectsFragment.getView();
+        ListView lv = v.findViewById(R.id.subjectsList);
+        lv.setAdapter( new ArrayAdapter<>(
+                        subjectsFragment.getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        subjects)
+        );
+        ProgressBar pb = v.findViewById(R.id.progressBar);
+        pb.setVisibility(View.GONE);
 
-    @Override
-    protected void onPostExecute(List list) {
-
-        if(loading){
-            progressBar.setVisibility(View.GONE);
-        }
-
-        super.onPostExecute(list);
     }
 }
--- app/src/main/java/com/unigrade/app/View/Fragment/SubjectsFragment.java
+++ app/src/main/java/com/unigrade/app/View/Fragment/SubjectsFragment.java
@@ -1,10 +1,7 @@
 package com.unigrade.app.View.Fragment;
-
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,35 +18,19 @@ import com.unigrade.app.View.Activity.MainActivity;
 import com.unigrade.app.View.AsyncTask.GetSubjects;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import androidx.navigation.Navigation;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SubjectsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SubjectsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SubjectsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private SubjectsController subjectsController;
-    private SwipeRefreshLayout swipeLayout;
-    private ProgressBar progressBar;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-    private List<Subject> subjects = new ArrayList<Subject>();
-    ArrayAdapter<Subject> adapter;
+    private ArrayList<Subject> subjects = new ArrayList<>();
+    private ProgressBar progressBar;
+    private ListView subjectList;
 
     public SubjectsFragment() {
         // Required empty public constructor
@@ -59,31 +40,10 @@ public class SubjectsFragment extends Fragment {
         this.subjects = subjects;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SubjectsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SubjectsFragment newInstance(String param1, String param2) {
-        SubjectsFragment fragment = new SubjectsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -94,14 +54,11 @@ public class SubjectsFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_subjects, container, false);
 
         progressBar = v.findViewById(R.id.progressBar);
-        ListView subjectList = v.findViewById(R.id.subjectsList);
+        subjectList = v.findViewById(R.id.subjectsList);
 
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Escolha a matéria");
 
-        adapter = new ArrayAdapter<Subject>(this.getActivity(),android.R.layout.simple_list_item_1, subjects);
-        subjectList.setAdapter(adapter);
-
-        callServer(true);
+        subjectList.setAdapter(new ArrayAdapter<>(this.getActivity(),android.R.layout.simple_list_item_1, subjects));
 
         subjectList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -113,57 +70,26 @@ public class SubjectsFragment extends Fragment {
         return v;
     }
 
-    private void callServer(Boolean loading){
+    private void callServer(){
          SubjectsController subjectsController = SubjectsController.getInstance();
 
         if(subjectsController.isConnectedToNetwork(getActivity())){
-            new GetSubjects(this, adapter, loading, progressBar).execute();
+            progressBar.setVisibility(View.VISIBLE);
+            new GetSubjects(this).execute();
         }else {
             Toast.makeText(this.getActivity(), "Não há conexão com a internet", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-        @Override
     public void onResume() {
-        callServer(true);
         super.onResume();
+        callServer();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
