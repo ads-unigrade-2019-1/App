d84b5796ffda6d8ed69ea12d03304c69b7ede38e
GeovanaRamos <geovannna.1998@gmail.com>
2019-04-26 16:03:51 -0300

    Added url for classes

--- app/src/main/java/com/unigrade/app/DAO/URLs.java
+++ app/src/main/java/com/unigrade/app/DAO/URLs.java
@@ -2,4 +2,5 @@ package com.unigrade.app.DAO;
 
 public class URLs {
     public static final String URL_ALL_SUBJECTS = "https://aw-flask-api.herokuapp.com/disciplinas";
+    public static final String URL_SUBJECT_CLASSES = "https://aw-flask-api.herokuapp.com/turmas";
 }
