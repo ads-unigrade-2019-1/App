8991371528eaf11a241aa3381326b4ffbfdbca5e
DanMke <danmke@hotmail.com>
2019-05-07 16:57:48 -0300

    Added URL of all subjects
    Signed-off-by: DanMke <danmke@hotmail.com>

--- app/src/main/java/com/unigrade/app/DAO/URLs.java
+++ app/src/main/java/com/unigrade/app/DAO/URLs.java
@@ -1,6 +1,6 @@
 package com.unigrade.app.DAO;
 
 public class URLs {
-    public static final String URL_ALL_SUBJECTS = "https://aw-flask-api.herokuapp.com/disciplinas";
+    public static final String URL_ALL_SUBJECTS = "https://mwapi.herokuapp.com/disciplines";
     public static final String URL_SUBJECT_CLASSES = "https://aw-flask-api.herokuapp.com/turmas";
 }
