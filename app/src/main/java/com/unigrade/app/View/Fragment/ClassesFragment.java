4534cd66f45040c171d4b875e59959d4ed6b82c9
Gabriela Medeiros <36926637+gabiMSilva@users.noreply.github.com>
2019-05-17 22:16:27 -0300

    Removed unused imports

--- app/src/main/java/com/unigrade/app/View/Fragment/ClassesFragment.java
+++ app/src/main/java/com/unigrade/app/View/Fragment/ClassesFragment.java
@@ -8,14 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.unigrade.app.Controller.ClassesController;
