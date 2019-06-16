1df897dbb6e8469f94caf34e661939436c1537d7
Guilherme Guy <guilhermeguy349@gmail.com>
2019-06-16 11:01:56 -0300

    Finished build/deploy configuration
    Co-authored-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- release_play_store.sh
+++ release_play_store.sh
@@ -2,7 +2,7 @@ MASTER_BRANCH="master"
 DEV_BRANCH="develop"
 
 # Are we on the right branch?
-if [ "$TRAVIS_BRANCH" = "$DEV_BRANCH" || "$TRAVIS_BRANCH" = "$MASTER_BRANCH"]; then
+if [  "$TRAVIS_BRANCH" = "$DEV_BRANCH" ] || [ "$TRAVIS_BRANCH" = "$MASTER_BRANCH" ]; then
   
   # Is this not a Pull Request?
   if [ "$TRAVIS_PULL_REQUEST" = false ]; then
@@ -10,23 +10,19 @@ if [ "$TRAVIS_BRANCH" = "$DEV_BRANCH" || "$TRAVIS_BRANCH" = "$MASTER_BRANCH"]; t
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
     
-      jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore key.jks app/build/outputs/apk/app-release-unsigned.apk unigrade
-      zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-signed.apk
+      jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore keys/keys.jks app/build/outputs/apk/release/app-release-unsigned.apk unigrade
+      $ANDROID_HOME/build-tools/28.0.3/zipalign -v 4 app/build/outputs/apk/release/app-release-unsigned.apk app/build/outputs/apk/release/app-release-signed.apk
       
-      supply init -j api-project-202893591038-55de682e5165.json -p com.unigrade.app
-      supply run -j api-project-202893591038-55de682e5165.json -p com.unigrade.app -b app/build/outputs/apk/app-release-signed.apk
-
-      if [ "$TRAVIS_BRANCH" = "$DEV_BRANCH"]; then
-      
-     
+      fastlane supply init -j keys/api-project-202893591038-55de682e5165.json -p com.unigrade.app
+    
+      if [ "$TRAVIS_BRANCH" = "$DEV_BRANCH" ]; then
+        fastlane supply run -j keys/api-project-202893591038-55de682e5165.json -p com.unigrade.app -b app/build/outputs/apk/release/app-release-signed.apk --track beta
       fi
     
-      if [ "$TRAVIS_BRANCH" = "$MASTER_BRANCH"]; then
-      
+      if [ "$TRAVIS_BRANCH" = "$MASTER_BRANCH" ]; then
+        fastlane supply run -j keys/api-project-202893591038-55de682e5165.json -p com.unigrade.app -b app/build/outputs/apk/release/app-release-signed.apk -- track production
       fi
-
-
-  fi
+    fi
   fi
 fi
 
