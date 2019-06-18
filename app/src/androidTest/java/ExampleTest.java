efa97ed561f65f445a4a999acccdbc8a5d43c729
Gabriela Medeiros <36926637+gabiMSilva@users.noreply.github.com>
2019-06-18 05:01:11 -0300

    Created test method

--- app/src/androidTest/java/ExampleTest.java
+++ app/src/androidTest/java/ExampleTest.java
@@ -1,14 +1,29 @@
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.robotium.solo.Solo;
+import com.unigrade.app.R;
 import com.unigrade.app.View.Activity.MainActivity;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
-public class ExampleTest extends ActivityTestRule<MainActivity> {
-    public ExampleTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+public class ExampleTest{
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Test
+    public void testBtnExample(){
+        onView(withId(R.id.btn_continue)).check(matches((isClickable())));
+        onView(withId(R.id.btn_continue)).perform(click());
     }
+
 }
