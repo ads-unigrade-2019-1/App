5dcb30349d0e5d35f23787bcadad4217a57181e9
Gabriela Medeiros <36926637+gabiMSilva@users.noreply.github.com>
2019-06-19 17:35:15 -0300

    Added remove class test

--- app/src/androidTest/java/ExampleTest.java
+++ app/src/androidTest/java/ExampleTest.java
@@ -3,6 +3,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.robotium.solo.Solo;
@@ -61,7 +62,7 @@ public class ExampleTest{
         solo.goBack();
         //vai para a tela de turmas adicionadas
         solo.clickOnView(solo.getView(R.id.userSubjectsFragment));
-        solo.sleep(2000);
+        solo.sleep(1000);
         //Clica na última matéria adicionada
         solo.clickInList(1);
         //salva o nome da matéria que foi adicionada
@@ -72,9 +73,50 @@ public class ExampleTest{
         assertTrue(solo.isCheckBoxChecked(0));
         //Verifica se a segunda turma est selecionada
         assertTrue(solo.isCheckBoxChecked(1));
+
+        solo.goBack();
+        assertTrue(solo.searchText((String)subjectName));
     }
 
-    //TODO teste de remoção de matéria
-    //TODO teste de geração de grade horária (com as turmas adicionadas)
-    //TODO teste de visualização do fluxo
+    @Test
+    public void removeUserClass(){
+        //vai para a pagina de add matérias
+        solo.sleep(2000);
+        solo.clickOnView(solo.getView(R.id.subjectsFragment));
+        solo.sleep(500);
+
+        //adiciona texto na pesquisa
+        solo.enterText(0, "testes");
+        solo.sleep(500);
+        //clica na primeira matéria encontrada
+        solo.clickInList(0);
+        solo.sleep(200);
+
+        //seleciona a primeira e a segunda turma da matéria
+        if(!solo.isCheckBoxChecked(0)){
+            solo.clickOnCheckBox(0);
+            solo.sleep(500);
+        }
+
+        solo.goBack();
+        //vai para a tela de turmas adicionadas
+        solo.clickOnView(solo.getView(R.id.userSubjectsFragment));
+        solo.sleep(1000);
+        //Clica na última matéria adicionada
+        solo.clickInList(1);
+        //salva o nome da matéria que foi adicionada
+        String subjectName = (String)((TextView)solo.getView(R.id.class_title)).getText();
+        //desmarca turmas anteriormente marcadas
+        if(solo.isCheckBoxChecked(0)){
+            solo.clickOnCheckBox(0);
+        }
+        solo.goBack();
+        //Verifica se a turma foi removida da lista de turmas do usuário
+        assertFalse(solo.searchText(subjectName));
+    }
+
+    //TODO teste de geração de grade horária (com pelo menos 1 turma adicionada pelo o usuário)
+    //TODO teste de inserção do curso
+    //TODO teste de visualização do fluxo (com o curso selecionado pelo usuário)
+
 }
